bfa75a42204eb8abb55f070d550aa5bff199126a
wlanchya <144409864+wlanchya@users.noreply.github.com>
2023-10-18 09:01:21 +0700

    Add files via upload

---
+++ pasien.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootsrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>MyApp | Halaman Utama</title>
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">MyApp</a>
+                <button class="navbar-toggler" type="button" data-bs- toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="pasien.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="pasien.php">Pasien</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="row mt-3">
+            <div class="col-sm">
+                <h3>Tabel Pasien</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <a href="tambahpasien.php" class="btn btn-primary btn-sm d-flex justify-content-center">Tambah Data</a>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col">
+                <table class="table table-striped table-hover table-sm">
+                    <tr>
+                        <th>No</th>
+                        <th>ID Pasien</th>
+                        <th>Nama Pasien</th>
+                        <th>Jenis Kelamin</th>
+                        <th>Alamat</th>
+                        <th>Action</th>
+                    </tr>
+                    <?php
+                        include 'koneksi.php';
+                        $no = 1;
+                        $hasil = $koneksi->query("SELECT * FROM pasien");
+                        ?>
+
+                    <?php
+                        while ($row = $hasil->fetch_assoc()) {
+                        ?>
+                    <tr>
+                        <td><?= $no++; ?></td>
+                        <td><?= $row['idPasien']; ?></td>
+                        <td><?= $row['nmPasien']; ?></td>
+                        <td><?= $row['jk']; ?></td>
+                        <td><?= $row['alamat']; ?></td>
+                        <td>
+                            <a href="editpasien.php?edit=<?= $row['idPasien']; ?>"
+                                class="btn btn-warning btn-sm">Edit</a>
+                            <a type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal"
+                                data-bs-target="#modalhapus">Hapus</a>
+                            <!-- Modal -->
+                            <div class="modal fade" id="modalhapus" tabindex="-1" aria-labelledby="exampleModalLabel"
+                                aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Hapus</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Anda yakin ingin menghapus data pasien  
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-bs-dismiss="modal">Close</button>
+                                            <a href="koneksi.php?idPasien=<?= $row['idPasien'] ?>"
+                                                class="btn btn-danger">Hapus</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </tr>
+                    <?php } ?>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- JavaScript Bootsrap -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
